8f17fe461498a97cf88cda3e259f424443f19975
Benji Wiebe <benjiwiebe14@gmail.com>
2014-04-28 21:06:26 +0000

    Rewrote parse_datetime

--- datetime.c
+++ datetime.c
@@ -52,79 +52,46 @@ time_t parse_date(char *str, char sep)
 
 time_t parse_datetime(char *str, char dsep, char tsep)
 {
-	/* format (assuming tsep == ':' and dsep == '-'): mm-dd-yyyyhh:mm */
-	int i = strlen(str);
-
-	if(i != 15)
-	{
+	/* format (2 is _tsep_, 1 is _dsep_): mm1dd1yyyyhh2mm */
+	if(strlen(str) != 15)
 		return FMT_ERR;
-	}
+
+	if(str[2] != dsep || str[5] != dsep || str[12] != tsep)
+		return FMT_ERR;
 
 	char syear[5];
 	char smonth[3];
 	char sday[3];
 	char shour[3];
 	char sminute[3];
-
-	syear[4] = 0;
-	smonth[2] = 0;
-	sday[2] = 0;
-	shour[2] = 0;
-	sminute[2] = 0;
-
 	int year = 0, month = 0, day = 0, hour = 0, minute = 0;
-	while(i--)
-	{
-		switch(i)
-		{
-			case 0:
-			case 1:
-				smonth[i] = str[i];
-				break;
-			case 3:
-			case 4:
-				sday[i - 3] = str[i];
-				break;
-			case 2:
-			case 5:
-				if(str[i] != dsep)
-				{
-					return FMT_ERR;
-				}
-				break;
-			case 6:
-			case 7:
-			case 8:
-			case 9:
-				syear[i - 6] = str[i];
-				break;
-			case 10:
-			case 11:
-				shour[i - 10] = str[i];
-				break;
-			case 12:
-				if(str[i] != tsep)
-				{
-					return FMT_ERR;
-				}
-				break;
-			case 13:
-			case 14:
-				sminute[i - 13] = str[i];
-				break;
-			default:
-				return FMT_ERR;
-				break;
-		}
-	}
 
+	strncpy(smonth, str, 2);
+	smonth[2] = 0;
 	satio(month, 12, 1);
+	str += 3;
+
+	strncpy(sday, str, 2);
+	sday[2] = 0;
 	satio(day, 31, 1);
+	str += 3;
+
+	strncpy(syear, str, 4);
+	syear[4] = 0;
 	satio(year, MAX_YEAR, MIN_YEAR);
+	str += 4;
+
+	strncpy(shour, str, 2);
+	shour[2] = 0;
 	satio(hour, 23, 0);
+	str += 3;
+
+	strncpy(sminute, str, 2);
+	sminute[2] = 0;
 	satio(minute, 59, 0);
 
-	struct tm *utm = malloc(sizeof(struct tm));
+		
+	struct tm *utm = calloc(1, sizeof(struct tm));
 
 	utm->tm_mday = day;
 	utm->tm_mon = month - 1;
@@ -134,12 +101,9 @@ time_t parse_datetime(char *str, char dsep, char tsep)
 	utm->tm_isdst = -1;
 
 	time_t t = mktime(utm);
-	//time_t t = (year * 12)
 	free(utm);
-	if(!(t > 0))
-	{
+	if(t == -1)
 		return FMT_ERR;
-	}
 	return t;
 }
 
