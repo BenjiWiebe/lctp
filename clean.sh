02f084c62d9b8d7cf1b1fbdd93625ef14eaefafb
Benji Wiebe <benjiwiebe14@gmail.com>
2014-05-01 23:27:53 +0000

    Added some basic but handy maintainer's scripts.

---
+++ clean.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+rm -rf missing install-sh test-driver config.guess config.status config.sub configure config.h.in config.h.in~ Makefile.in lib/Makefile.in tests/Makefile.in depcomp aclocal.m4 autom4te.cache
