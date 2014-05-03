64a6b1123a98efec549970d38c383cc78d11af8f
Benji Wiebe <benjiwiebe14@gmail.com>
2014-05-03 16:58:55 +0000

    Added test results to list of files to delete

--- clean.sh
+++ clean.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-rm -rf missing install-sh test-driver config.guess config.status config.sub configure config.h.in config.h.in~ Makefile.in lib/Makefile.in tests/Makefile.in depcomp aclocal.m4 autom4te.cache
+rm -rf tests/*.sh.log tests/*.sh.trs missing install-sh test-driver config.guess config.status config.sub configure config.h.in config.h.in~ Makefile.in lib/Makefile.in tests/Makefile.in depcomp aclocal.m4 autom4te.cache
