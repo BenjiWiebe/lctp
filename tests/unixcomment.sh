f4fae9255529d428ba7d67aa1f8084964582fd55
Benji Wiebe <benjiwiebe14@gmail.com>
2015-02-09 19:23:23 +0000

    Fixed minor bug

--- tests/unixcomment.sh
+++ tests/unixcomment.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-result=$(../lctp --no-warnings -q ${srcdir}/twomonths_unixcomment.dat)
+result=$(../lctp --no-warnings -q ${srcdir}/unixcomment.dat)
 if test "x${result}" = "x535.70"
 then
 	exit 0
